2bf1ecd9bd8a9e1893dd6e136deeca7ae1ea5d51
Mikhail <kami@slink.ws>
2024-06-13 22:40:47 +1000

    initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/slink-go/util
+
+go 1.22.3
+
+toolchain go1.22.4
+
+require (
+	github.com/slink-go/logging v0.0.2
+	github.com/xhit/go-str2duration/v2 v2.1.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+)
---
+++ matcher/matcher.go
@@ -0,0 +1,54 @@
+package matcher
+
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type PatternMatcher interface {
+	Matches(input string) bool
+	MatchesExact(input, exact string) bool
+}
+
+func NewRegexPatternMatcher(patterns ...string) PatternMatcher {
+	pm := regexPatternMatcher{}
+	if len(patterns) > 0 {
+		sort.Slice(patterns, func(i, j int) bool {
+			l1, l2 := len(patterns[i]), len(patterns[j])
+			if l1 != l2 {
+				return l1 < l2
+			}
+			return patterns[i] < patterns[j]
+		})
+	}
+	var result []*regexp.Regexp
+	for _, pattern := range patterns {
+		result = append(result, regexp.MustCompile(strings.ReplaceAll(pattern, "*", ".*")))
+	}
+	pm.regularExpressions = result
+	return &pm
+}
+
+type regexPatternMatcher struct {
+	regularExpressions []*regexp.Regexp
+}
+
+func (pm *regexPatternMatcher) Matches(input string) bool {
+	if pm.regularExpressions == nil || len(pm.regularExpressions) == 0 {
+		return true
+	}
+	for _, regularExpression := range pm.regularExpressions {
+		if regularExpression.MatchString(input) {
+			return true
+		}
+	}
+	return false
+}
+func (pm *regexPatternMatcher) MatchesExact(input, exact string) bool {
+	if strings.Contains(exact, "*") {
+		return pm.Matches(input)
+	} else {
+		return input == exact
+	}
+}
